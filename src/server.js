1f18022395038813db805b83e9083fc62fe436f2
SanderTLN <demihhovski@mail.ru>
2021-10-08 13:35:38 +0300

    As a user I can log in and out [Delivers #179863711]

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
+
+const FileStore = sessionFileStore(session);
 
 const { json } = require('body-parser');
 const { PORT, NODE_ENV } = process.env;
@@ -12,7 +16,23 @@ polka() // You can also use Express
 		compression({ threshold: 0 }),
 		json(),
 		sirv('static', { dev }),
-		sapper.middleware()
+		session({
+			secret: 'bleh',
+			resave: false,
+			saveUnitialized: true,
+			cookie: {
+				maxAge: 31536000
+			},
+			store: new FileStore({
+				path: '.sessions'
+			}),
+		}),
+		sapper.middleware({
+			session: req => ({
+				user: req.session && req.session.user,
+				token: req.session && req.session.token
+			})
+		})
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
